f9c5914efd3e14c54c53c76b89cc9b6ec8c97938
Eric Mesa <ericsbinaryworld@gmail.com>
2019-10-24 19:14:55 -0400

    Enemy moves on screen now

--- Assets/Scripts/EnemyPathing.cs
+++ Assets/Scripts/EnemyPathing.cs
@@ -6,16 +6,39 @@ public class EnemyPathing : MonoBehaviour
 {
 
     [SerializeField] List<Transform> waypoints; // why type transform? Because we're looking at the position
+    [SerializeField] float moveSpeed = 2f;
+
+    int waypointIndex = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform.position = waypoints[waypointIndex].transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
+    }
+
+    private void Move()
+    {
+        if (waypointIndex <= waypoints.Count - 1)
+        {
+            var targetPosition = waypoints[waypointIndex].transform.position;
+            var movementThisFrame = moveSpeed * Time.deltaTime;
+            transform.position = Vector2.MoveTowards
+                (transform.position, targetPosition, movementThisFrame);
+
+            if (transform.position == targetPosition)
+            {
+                waypointIndex++;
+            }
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 }
